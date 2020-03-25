43bfce7355433711a890833ff190ae29922b6418
Adrián Pardini <github@tangopardo.com.ar>
2020-03-25 10:29:38 -0300

    Adds basic firmware skeleton

---
+++ firmware/src/main.ino
@@ -0,0 +1,22 @@
+#include "talon6.h"
+
+Talon6::Parser parser;
+Talon6::Roof roof;
+Talon6::ResponseEncoder response_encoder(roof);
+
+Talon6::Commands cmd = Talon6::Commands::NO_COMMAND;
+const char *response;
+
+
+void setup ()
+{
+
+
+}
+
+
+void loop ()
+{
+
+
+}
